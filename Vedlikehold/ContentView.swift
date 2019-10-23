4f336e83fe3ea09bc127d20c401665149822280a
Jan Hovland <jan.hovland@lyse-net>
2019-10-23 18:52:14 +0200

    Update

--- Vedlikehold/ContentView.swift
+++ Vedlikehold/ContentView.swift
@@ -24,102 +24,115 @@ struct ContentView : View {
 
     var body: some View {
         NavigationView {
-            Form {
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("First name")
-                               .font(.footnote)
-                               .padding(-3)
-                            TextField("Enter the first name", text: $firstName)
-                        }
+            List {
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("First name")
+                           .font(.footnote)
+                           .padding(-3)
+                        TextField("Enter the first name", text: $firstName)
+                            .padding(-7)
+                            .padding(.horizontal, 15)
                     }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("Last name")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the last name", text: $lastName)
-                        }
-                    }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("eMail")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the email address", text: $personEmail)
-                        }
-                    }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("Address")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the address", text: $address)
-                        }
-                    }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("Phone Number")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the phone number", text: $phoneNumber)
-                        }
-                    }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("City")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the city", text: $city)
-                                                   }
-                    }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("Municipality")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the municipality", text: $municipality)
-                            .fixedSize()
-                        }
-                    }
-                    
-                    ZStack {
-                        VStack (alignment: .leading) {
-                            Text("Date of birth")
-                               .font(.footnote)
-                               .padding(-5)
-                            TextField("Enter the dateOfBirth", text: $dateOfBirth)
-                        }
-                    }
+                }
                 
-                    VStack {
-                        HStack {
-                            Text("Gender")
-                            .font(.footnote)
-                            .padding(-5)
-                            Picker(selection: $gender, label: Text("")) {
-                                ForEach(0..<genders.count) { index in
-                                    Text(self.genders[index]).tag(index)
-                                }
-                            }
-                            .pickerStyle(SegmentedPickerStyle())
-                            .padding(5)
-                        }
-                    
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("Last name")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the last name", text: $lastName)
+                        .padding(-7)
+                        .padding(.horizontal, 15)
                     }
-            }
+                }
+                                   
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("eMail")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the email address", text: $personEmail)
+                           .padding(-7)
+                           .padding(.horizontal, 15)
+                    }
+                }
                 
-             
-            .navigationBarTitle(Text("Person opplysninger"), displayMode: .inline)
-            .padding(-5)
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("Address")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the address", text: $address)
+                        .padding(-7)
+                        .padding(.horizontal, 15)
+                    }
+                }
+                
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("Phone Number")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the phone number", text: $phoneNumber)
+                        .padding(-7)
+                        .padding(.horizontal, 15)
+                    }
+                }
+                
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("City")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the city", text: $city)
+                        .padding(-7)
+                        .padding(.horizontal, 15)
+                    }
+                }
+                
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("Municipality")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the municipality", text: $municipality)
+                          .fixedSize()
+                          .padding(-7)
+                          .padding(.horizontal, 15)
+                    }
+                }
+                
+                ZStack {
+                    VStack (alignment: .leading) {
+                        Text("Date of birth")
+                           .font(.footnote)
+                           .padding(-5)
+                        TextField("Enter the dateOfBirth", text: $dateOfBirth)
+                        .padding(-7)
+                        .padding(.horizontal, 15)
+                    }
+                }
             
+                VStack {
+                    HStack {
+                        Text("Gendercccccc")
+                        .font(.footnote)
+                        .padding(-5)
+                        Picker(selection: $gender, label: Text("")) {
+                            ForEach(0..<genders.count) { index in
+                                Text(self.genders[index]).tag(index)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Person opplysninger"), displayMode: .inline)
         }
+        // Removes all separators below
+        .listStyle(GroupedListStyle())
+         
     }
 }
 
