26458083ecf21b9594d1a6516580c4aa910d1e99
Jan Hovland <jan.hovland@lyse-net>
2019-10-24 10:37:12 +0200

    Update

--- Vedlikehold/ContentView.swift
+++ Vedlikehold/ContentView.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2019 Jan Hovland. All rights reserved.
 //
 
+// Short cuts made by me:
+// Ctrl + Cmd + / (on number pad)  == Comment or Uncomment
+// Ctrl + Cmd + * (on number pad)  == Indent
+
 import SwiftUI
 
 struct ContentView : View {
@@ -21,7 +25,7 @@ struct ContentView : View {
     
     @State var gender = 0
     var genders = ["Man", "Women"]
-
+    
     var body: some View {
         NavigationView {
             List {
@@ -33,26 +37,29 @@ struct ContentView : View {
                 InputTextField(heading: "Phone Number", placeHolder: "Enter the phone number",  value: $phoneNumber)
                 
                 HStack {
-                    InputTextField(heading: "City",         placeHolder: "Enter the city",          value: $city)
-                    InputTextField(heading: "Municipality", placeHolder: "Enter the municipality",  value: $municipality)
+                    InputTextField(heading: "City", placeHolder: "Enter the city", value: $city)
                     Image(systemName: "magnifyingglass")
-                      .foregroundColor(.blue)
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .foregroundColor(.blue)
                         // .font(Font.system(.title).bold())
                         // .font(Font.system(.headline))
                         // .font(Font.system(.title))
                         .font(.title)
                 }
                 
+                InputTextField(heading: "Municipality", placeHolder: "Enter the municipality",  value: $municipality)
+                
                 InputDate(heading: "Date of birth", placeHolder: "Enter the dateOfBirth", value: $dateOfBirth)
-              
+                
                 // Returning an inteher 0 == "Man" 1 == "Women
                 InputGender(heading: "Gender ", genders: genders, value: $gender)
                 
             }
             .navigationBarTitle(Text("Personal information"), displayMode: .inline)
         }
-        // Removes all separators below in the List view
-        .listStyle(GroupedListStyle())
+            // Removes all separators below in the List view
+            .listStyle(GroupedListStyle())
     }
 }
 
@@ -64,62 +71,62 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct InputTextField: View {
-   var heading: String
-   var placeHolder: String
-   @Binding var value: String
-
-   var body: some View {
-      ZStack {
-          VStack (alignment: .leading) {
-              Text(heading)
-                 .font(.footnote)
-                 .padding(-5)
-              TextField(placeHolder, text: $value)
-                 .padding(-7)
-                 .padding(.horizontal, 15)
-          }
-       }
+    var heading: String
+    var placeHolder: String
+    @Binding var value: String
+    
+    var body: some View {
+        ZStack {
+            VStack (alignment: .leading) {
+                Text(heading)
+                    .font(.footnote)
+                    .padding(-5)
+                TextField(placeHolder, text: $value)
+                    .padding(-7)
+                    .padding(.horizontal, 15)
+            }
+        }
     }
 }
 
 struct InputDate: View {
-   var heading: String
-   var placeHolder: String
-   @Binding var value: String
-
-   var body: some View {
-      ZStack {
-          VStack (alignment: .leading) {
-              Text(heading)
-                 .font(.footnote)
-                 .padding(-5)
-              TextField(placeHolder, text: $value)
-              .padding(-7)
-              .padding(.horizontal, 15)
-              .keyboardType(.numbersAndPunctuation)
-          }
-      }
-   }
+    var heading: String
+    var placeHolder: String
+    @Binding var value: String
+    
+    var body: some View {
+        ZStack {
+            VStack (alignment: .leading) {
+                Text(heading)
+                    .font(.footnote)
+                    .padding(-5)
+                TextField(placeHolder, text: $value)
+                    .padding(-7)
+                    .padding(.horizontal, 15)
+                    .keyboardType(.numbersAndPunctuation)
+            }
+        }
+    }
 }
 
 struct InputGender: View {
-   var heading: String
-   var genders: [String]
-   @Binding var value: Int
-
-   var body: some View {
-      VStack {
-          HStack {
-              Text(heading)
-              .font(.footnote)
-              .padding(-5)
-              Picker(selection: $value, label: Text("")) {
-                  ForEach(0..<genders.count) { index in
-                      Text(self.genders[index]).tag(index)
-                  }
-              }
-              .pickerStyle(SegmentedPickerStyle())
-          }
-      }
-   }
+    var heading: String
+    var genders: [String]
+    @Binding var value: Int
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(heading)
+                    .font(.footnote)
+                    .padding(-5)
+                Picker(selection: $value, label: Text("")) {
+                    ForEach(0..<genders.count) { index in
+                        Text(self.genders[index]).tag(index)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+        }
+    }
 }
